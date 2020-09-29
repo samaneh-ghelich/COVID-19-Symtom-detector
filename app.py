0bbafed882d28f3419425769237aa6cdeae0bd21
samaneh-ghelich <69047810+samaneh-ghelich@users.noreply.github.com>
2020-09-29 11:21:46 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict_proba(final_features)
+
+    output = round(prediction[0][1]*100,2)
+    return render_template('output.html', prediction_text=output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
